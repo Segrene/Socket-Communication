9e6c460b8839e14c35ad0104179865bb3df45adc
LAPTOP-SGRN\gbsiw <gbsiwoo@gmail.com>
2024-12-10 13:40:16 +0900

    SocketCommunication 자동 모드 구현 소켓 입출력 개선 RCoord string을 이용한 좌표 입력 추가 클래스 생성자 개선 및 정리

--- Socket Communication/RCoord.h
+++ Socket Communication/RCoord.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <array>
 
@@ -14,7 +15,14 @@ public:
 	RCOORD() {
 		Coord1 = { 0,0,0,0,0,0 }; Coord2 = { 0,0,0,0,0,0 }; length = 0;
 	}
-	RCOORD(double x, double y, double z, double rx, double ry, double rz) { Coord1 = {x,y,z,rx,ry,rz}; }
+	RCOORD(double x, double y, double z, double rx, double ry, double rz) { Coord1 = { x,y,z,rx,ry,rz }; Coord2 = { x,y,z,rx,ry,rz }; length = 0; }
+	RCOORD(double coord1[], double coord2[]) {
+		for (int i = 0; i < 6; i++) {
+			Coord1[i] = coord1[i];
+			Coord2[i] = coord2[i];
+		}
+		length = 0;
+	}
 
 	void setCoord1(double x, double y, double z, double rx, double ry, double rz) { Coord1 = { x,y,z,rx,ry,rz }; }
 	void setCoord2(double x, double y, double z, double rx, double ry, double rz) { Coord2 = { x,y,z,rx,ry,rz }; }
@@ -26,6 +34,32 @@ public:
 	void setCoord2(double coord[]) {
 		for (int i = 0; i < 6; i++) {
 			Coord2[i] = coord[i];
+		}
+	}
+	void setCoord1(string coord) {
+		istringstream ss(coord);
+		array<string, 6> coordString;
+		int i = 0;
+		string buffer;
+		while (getline(ss, buffer, ',')) {
+			coordString[i] = buffer;
+			i++;
+		}
+		for (int j = 0; j < 6; j++) {
+			Coord1[j] = stod(coordString[j]);
+		}
+	}
+	void setCoord2(string coord) {
+		istringstream ss(coord);
+		array<string, 6> coordString;
+		int i = 0;
+		string buffer;
+		while (getline(ss, buffer, ',')) {
+			coordString[i] = buffer;
+			i++;
+		}
+		for (int j = 0; j < 6; j++) {
+			Coord2[j] = stod(coordString[j]);
 		}
 	}
 	string getCoord1String() {
@@ -42,7 +76,7 @@ public:
 		std::string coordString;
 		for (int i = 0; i < 5; i++) {
 			coordString += to_string(Coord2[i]);
-			coordString += ", ";
+			coordString += ",";
 		}
 		coordString += to_string(Coord2[5]);
 		coordString += "\n";
--- Socket Communication/SocketCommunication.cpp
+++ Socket Communication/SocketCommunication.cpp
@@ -1,10 +1,12 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 
 #include <stdio.h>
+#include <conio.h>
 #include <WinSock2.h>
 #include <Windows.h>
 #include <iostream>
 #include <string>
+#include <thread>
 #include "RCoord.h"
 
 #pragma comment(lib, "ws2_32")
@@ -14,22 +16,31 @@
 #define PORT 200
 #define PACKET_SIZE 1024
 
-std::string RecvMessage(SOCKET hClient, char* cBuffer, std::string& RecvString);
-int SendMessage(SOCKET hClient, std::string& SendMsg);
+using namespace std;
+
+int RecvMessage(SOCKET hClient, char* cBuffer, string& RecvString);
+string GetString();
+int SendMessage(SOCKET hClient, string& SendMsg);
+int NotReady(string& RecvString);
+int AutoMode(SOCKET hClient, char* cBuffer, string& RecvString, string& SendMsg);
+void MoveBegin(RCOORD& coord, char* cBuffer, string& RecvString, string& SendMsg);
+void MoveEnd(RCOORD& coord, char* cBuffer, string& RecvString, string& SendMsg);
+int getCommand();
+
+int key;
 
 int main() {
-	RCOORD Coord1(0,1,2,3,4,5);
-	std::cout << Coord1.getCoord1String();
+	/*RCOORD Coord1(10,20,30,20,10,0);
+	cout << Coord1.getCoord1String();
 	double Tcoord[6] = {9,8,7,6,5,4};
 	Coord1.setCoord2(Tcoord);
-	std::cout << Coord1.getCoord2String();
+	cout << Coord1.getCoord2String();
 	double Acoord1[6] = { 0 };
 	double Acoord2[6] = { 0 };
 	Coord1.getCoord1(Acoord1);
 	Coord1.getCoord2(Acoord2);
-	std::cout << std::to_string(Acoord1[1]) << std::endl;
-	std::cout << std::to_string(Acoord2[1]) << std::endl;
-	return 0;
+	cout << to_string(Acoord1[1]) << endl;
+	cout << to_string(Acoord2[1]) << endl;*/
 
 	WSADATA wsadata;
 	WSAStartup(MAKEWORD(2, 2), &wsadata);
@@ -50,27 +61,42 @@ int main() {
 	SOCKET hClient = accept(hListen, (SOCKADDR*)&tCIntAddr, &iCIntSize);
 
 	char cBuffer[PACKET_SIZE] = {};
-	std::string RecvMsg = "";
-	std::string& RecvString = RecvMsg;
-	std::string cMsg = "";
-	std::string& SendMsg = cMsg;
+	string RecvMsg = "";
+	string& RecvString = RecvMsg;
+	string cMsg = "";
+	string& SendMsg = cMsg;
+	int A = 0;
 
 	RecvMessage(hClient, cBuffer, RecvString);
 
 	while (1) {
-		SendMessage(hClient, SendMsg);
+		if (A == 1) {
+			A = AutoMode(hClient, cBuffer, RecvString, SendMsg); //자동 모드 진입
+		}
+		if (A == -1) {
+			break;
+		}
+
+		cMsg = GetString();
 		if (cMsg.compare("end") == 0) {
+			SendMessage(hClient, SendMsg);
 			break;
+		} else if (cMsg.compare("manual") == 0) {
+			A = 0;
+			continue;
+		} else if (cMsg.compare("auto") == 0) {
+			A = 1;
+			continue;
 		}
+		SendMessage(hClient, SendMsg);
 
-		system("cls");
+		//system("cls");
 		RecvMessage(hClient, cBuffer, RecvString);
-		if (RecvMsg.compare("Ready") == -1) {
-			std::cout << "Task Failure" << std::endl;
+		if (NotReady(RecvString)) {
 			break;
 		}
 
-		std::cout << std::endl;
+		cout << endl;
 	}
 
 	RecvMessage(hClient, cBuffer, RecvString);
@@ -82,18 +108,73 @@ int main() {
 	return 0;
 }
 
-std::string RecvMessage(SOCKET hClient, char* cBuffer, std::string& RecvString) {
-	recv(hClient, cBuffer, PACKET_SIZE, 0);
-	RecvString = cBuffer;
-	memset(cBuffer, 0, sizeof(char) * PACKET_SIZE);
-	std::cout << "RecvMsg : " << RecvString << std::endl;
-	RecvString = "";
-	return RecvString;
+string GetString() {
+	string gstr;
+	cout << "SendMsg : ";
+	cin >> gstr;
+	return gstr;
 }
 
-int SendMessage(SOCKET hClient, std::string& SendMsg) {
-	std::cout << "SendMsg : ";
-	std::cin >> SendMsg;
+int RecvMessage(SOCKET hClient, char* cBuffer, string& RecvString) {
+	RecvString = ""; //받은 메세지 초기화
+	recv(hClient, cBuffer, PACKET_SIZE, 0); //메세지 수신
+	RecvString = cBuffer; //char 에서 string으로
+	memset(cBuffer, 0, sizeof(char) * PACKET_SIZE); //char 버퍼 초기화
+	cout << "RecvMsg : " << RecvString << endl; //받은 메세지 출력
+	return 1;
+}
+
+int NotReady(string& RecvString) {
+	if (RecvString.compare("Ready\r\n") != 0) { //Ready가 아닌 메세지를 받을 경우 종료
+		cout << "Task Failure" << endl;
+		return 1;
+	}
+	return 0;
+}
+
+int SendMessage(SOCKET hClient, string& SendMsg) {
 	send(hClient, SendMsg.c_str(), strlen(SendMsg.c_str()), 0);
 	return 1;
+}
+
+int AutoMode(SOCKET hClient, char* cBuffer, string& RecvString, string& SendMsg) {
+	//RCOORD Coord1(0,-30,-20,0,0,0); //임시로 만든 좌표 변수. 나중에 분리해서 매개변수로 받을 예정
+	//double c2[] = { 0,30,-20,0,0,0 };
+	//Coord1.setCoord2(c2);
+	RCOORD Coord1;
+	string c1 = GetString();
+	string c2 = GetString();
+	Coord1.setCoord1(c1);
+	Coord1.setCoord2(c2);
+	while (1) {
+		SendMsg = Coord1.getCoord1String();
+		SendMessage(hClient, SendMsg); //좌표 1 전송
+		RecvMessage(hClient, cBuffer, RecvString); //Ready대기
+		if (NotReady(RecvString)) {
+			return -1;
+		}
+		SendMsg = Coord1.getCoord2String(); //좌표 2 전송
+		SendMessage(hClient, SendMsg);
+		RecvMessage(hClient, cBuffer, RecvString);
+		if (NotReady(RecvString)) {
+			return -1;
+		}
+		if (getCommand() != -1) {
+			return 0;
+		}
+	}
+}
+
+void MoveBegin(RCOORD& coord, char* cBuffer, string& RecvString, string& SendMsg) {
+	SendMsg = coord.getCoord1String();
+}
+void MoveEnd(RCOORD& coord, char* cBuffer, string& RecvString, string& SendMsg) {
+	SendMsg = coord.getCoord2String();
+}
+
+int getCommand() { //실시간으로 키 입력을 받는 함수
+	if (_kbhit()) {
+		return _getch();
+	}
+	return -1;
 }
